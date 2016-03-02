b65b790322cbf9bcd63910d4eb96eda70646d476
k1785 <k1785@rambler.ru>
2016-03-02 13:13:24 +0300

    Add ajax points get

--- app/ajax/points.php
+++ app/ajax/points.php
@@ -1,13 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Игорёк
- * Date: 01.03.2016
- * Time: 21:44
- */
 $points = array();
 $points['points'] = array();
-$points['points'] = array('x' => '20%', 'y' => '0%', 'text' => '<p>Точка 1</p>', 'start' => 10, 'end' => 15);
-$points['points'] = array('x' => '20%', 'y' => '0%', 'text' => '<p>Точка 2</p>', 'start' => 20, 'end' => 30);
-$points['points'] = array('x' => '20%', 'y' => '100%', 'text' => '<p>Точка 3</p>', 'start' => 60, 'end' => 80);
+$points['points'][] = array('type' => 'left','x' => '0%', 'y' => '0%', 'content' => '<p>Точка 1</p>', 'start' => 5, 'end' => 10);
+$points['points'][] = array('type' => 'up','x' => '0%', 'y' => '0%', 'content' => '<p>Точка 2</p>', 'start' => 20, 'end' => 30);
+$points['points'][] = array('type' => 'right','x' => '80%', 'y' => '50%', 'content' => '<p>Точка 3</p>', 'start' => 60, 'end' => 80);
+$points['points'][] = array('type' => 'down','x' => '20%', 'y' => '50%', 'content' => '<p>Точка 4</p>', 'start' => 70, 'end' => 90);
 exit(json_encode($points));
--- app/scripts/controllers/points.js
+++ app/scripts/controllers/points.js
@@ -1,15 +1,51 @@
 'use strict';
 angular.module('myApp').controller('PointsCtrl',
-    function ($sce) {
+    function ($sce,$http) {
         this.consoleCuePointsMessages = "click play to see cue points bindings!\n";
         this.API = null;
         this.barChartStyle = {};
         this.textStyle = {};
         this.chapterSelected = {};
 
+        this.pointsUrl = '/ajax/points.php';
+
+        this.points = [];
+
+        var ctrl = this;
+
         this.onPlayerReady = function (API) {
             this.API = API;
+            var data = {'id' : 11, 'user' : 10};
+            var config = {}
+            $http.post(this.pointsUrl, data, config).then(function(response){
+                ctrl.setPoints(response.data.points);
+            }, function(){
+            });
         };
+
+        this.onPlayerReadyRight = function (API) {
+            this.API = API;
+        }
+
+        this.config = {};
+        this.config.sourcesRight = [
+            {
+                /*src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.mp4"),*/
+                src: $sce.trustAsResourceUrl("/assets/video/videogular.mp4"),
+                type: "video/mp4"
+            },
+            /*{
+             src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.ogg"),
+             type: "video/ogg"
+             },
+             {
+             src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.webm"),
+             type: "video/webm"
+             }*/
+        ]
+
+
+
 
         this.media = [
             {
@@ -28,82 +64,50 @@ angular.module('myApp').controller('PointsCtrl',
                         type: "video/webm"
                     }*/
                 ],
-                tracks: [
-                    {
-                        src: "assets/subs/pale-blue-dot.vtt",
-                        kind: "captions",
-                        srclang: "en",
-                        label: "English",
-                        default: "default"
-                    }
-                ]
             }
         ];
 
-        // Console
-        this.onConsoleCuePoint = function onConsoleCuePoint(currentTime, timeLapse, params) {
-            var percent = (currentTime - timeLapse.start) * 100 / (timeLapse.end - timeLapse.start);
-            this.consoleCuePointsMessages = "time: " + currentTime + " -> (start/end/percent) " + timeLapse.start + "/" + timeLapse.end + "/" + percent + "% = " + params.message + "\n";
-        };
-
-        // Animations
-        this.onEnterAnimationsCuePoint = function onLeaveAnimationsCuePoint(currentTime, timeLapse, params) {
-            console.log("entering in animation!");
-            params.prop[params.value] = "0" + params.units;
-        };
-
-        this.onLeaveAnimationsCuePoint = function onLeaveAnimationsCuePoint(currentTime, timeLapse, params) {
-            params.prop[params.value] = "0" + params.units;
-        };
-
-        this.onUpdateAnimationsCuePoint = function onUpdateAnimationsCuePoint(currentTime, timeLapse, params) {
-            var percent = (currentTime - timeLapse.start) * 100 / (timeLapse.end - timeLapse.start);
-            var value = params.final * percent / 100;
-
-            params.prop[params.value] = value + params.units;
-        };
-
-        this.onCompleteAnimationsCuePoint = function onCompleteAnimationsCuePoint(currentTime, timeLapse, params) {
-            params.prop[params.value] = params.final + params.units;
-        };
+        this.defaulPointInterval = 5;
+        //status points
+        this.statusPoints = function(currentTime) {
+            var status = 0;
+            for (var i = 0, l = this.points.length; i < l; i++) {
+                if (!this.points[i].end) {
+                    this.points[i].end = this.points[i].start + this.defaulPointInterval;
+                }
+                if(currentTime >= this.points[i].start) {
+                    if (!!this.points[i].end) {
+                        if (currentTime > this.points[i].end) {
+                            status = 0;
+                        }else{
+                            status = 1;
+                        }
+                    } else {
+                        status = 0;
+                    }
+                }else{
+                    status = 0;
+                }
+                this.points[i].status = status;
+            }
+        }
 
         // Chapters
         this.onChaptersCuePoint = function onChaptersCuePoint(currentTime, timeLapse, params) {
-            for (var i = 0, l = this.points.length; i < l; i++) {
-                if(i != params.index){
-                    if(currentTime >= this.points[i].start) {
-                        if (!!this.points[i].end) {
-                            if (currentTime > this.points[i].end) {
-                                this.points[i].status = 0;
-                            }
-                        } else {
-                            this.points[i].status = 0;
-                        }
-                    }else{
-                        this.points[i].status = 0;
-                    }
-                }
-            }
-            var status = 0;
-            if(currentTime < timeLapse.end){
-                status = 1;
-            }
-            this.points[params.index].status = status;
             this.chapterSelected = this.config.cuePoints.chapters[params.index];
         };
+
+        this.onUpdateTime = function (currentTime, totalTime) {
+            this.statusPoints(currentTime);
+        };
+
 
         this.onChangeChapter = function onChangeChapter() {
             this.API.seekTime(this.chapterSelected.value);
         };
 
-        this.points = [
-            {type:'left', x : '0%', y: '0%', content : '<p>Точка 1</p>',label : '1', start : 7, status : 0},
-            {type:'right', x : '50%', y: '0%', content : '<p>Точка 2</p>',label : '2', start: 14, status : 0},
-            {type:'top', x : '90%', y: '90%', content : '<p>Точка 3</p>', label : '3', start: 30, end: 40, status : 0},
-            {type:'left', x : '50%', y: '90%', content : '<p>Точка 4</p>', label : '4', start: 35, end: 50, status : 0},
-        ];
-
         var actions = [
+            'comment',
             'left',
             'left_up',
             'up',
@@ -120,25 +124,33 @@ angular.module('myApp').controller('PointsCtrl',
             this.menu.push({'icon' : action, 'url' : '/points/'+action,});
         }
 
-        var _chapters = [];
-
-        for (var i = 0, l = this.points.length; i < l; i++) {
-            var point = this.points[i];
-            var chapter = {
-                timeLapse: {
-                    start: point.start
-                },
-                onEnter: this.onChaptersCuePoint.bind(this),
-                onLeave: this.onChaptersCuePoint.bind(this),
-                onUpdate: this.onChaptersCuePoint.bind(this),
-                onComplete: this.onChaptersCuePoint.bind(this),
-            };
-            chapter.params = point;
-            chapter.params.index = i;
-            if(!!point.end){
-                chapter.timeLapse.end = point.end;
+        this.setPoints = function(points){
+            this.points = points;
+            this.setcuePoints();
+        }
+        this.setcuePoints = function(){
+            var _chapters = [];
+            for (var i = 0, l = this.points.length; i < l; i++) {
+                var point = this.points[i];
+                var chapter = {
+                    timeLapse: {
+                        start: point.start
+                    },
+                    onEnter: this.onChaptersCuePoint.bind(this),
+                    onLeave: this.onChaptersCuePoint.bind(this),
+                    onUpdate: this.onChaptersCuePoint.bind(this),
+                    onComplete: this.onChaptersCuePoint.bind(this),
+                };
+                chapter.params = point;
+                chapter.params.index = i;
+                if(!!point.end){
+                    chapter.timeLapse.end = point.end;
+                }else{
+                    chapter.timeLapse.end = point.start + this.defaulPointInterval
+                }
+                _chapters.push(chapter);
             }
-            _chapters.push(chapter);
+            this.config.cuePoints  = {chapters : _chapters};
         }
 
         this.thumbnails = "assets/thumbnails/thumbnail.jpg";
@@ -155,17 +167,13 @@ angular.module('myApp').controller('PointsCtrl',
             theme: {
                 url: "styles/themes/points/videogular.css"
             },
-            cuePoints: {
-                console : [],
-                chapters: _chapters,
-                thumbnails: []
-            },
             plugins: {
                 poster: {
                     url: "assets/images/videogular.png"
                 }
             },
         };
+        this.setcuePoints();
     }
 ).directive("pointsMapPlugin",
     ["VG_STATES", function(VG_STATES) {
